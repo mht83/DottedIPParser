45fe0b8895373d5b3d23905d6686bc60508d9319
Mohammadhassan Tohidi <tohidihassan9@gmail.com>
2024-04-18 17:54:45 +0330

    Add project files.

---
+++ IP4AddressConsoleIO.cpp
@@ -0,0 +1,56 @@
+﻿//////////////////////////////////////////////written by mht83/////////////////////////////////////////
+#include "IP4AddressConsoleIO.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+class IPv4 {
+private:
+    std::string dottedIp;
+    int addressOctets[4] = { 0, 0, 0, 0 };
+
+public:
+
+    void splitDottedIp(const std::string& ip) {
+        std::string octet = "";
+        int index = 0;
+        for (char ch : ip) {
+            if (ch != '.') {
+                octet += ch;
+            }
+            else {
+                addressOctets[index] = std::stoi(octet);
+                index++;
+                octet = "";
+            }
+        }
+        addressOctets[3] = std::stoi(octet); 
+    }
+
+    void readDottedIp() {
+        std::string inputIp;
+        std::cin >> inputIp;
+        dottedIp = inputIp;
+
+        splitDottedIp(dottedIp);
+    }
+
+
+    void displayAddress() {
+        std::cout << "( ";
+        for (int i = 0; i < 4; ++i) {
+            std::cout << addressOctets[i];
+            if (i < 3) std::cout << " , ";
+        }
+        std::cout << " )"<<std::endl;
+    }
+};
+
+int main() {
+    IPv4 ip;
+    std::cout << "Please enter an IP address in dotted format: ";
+    ip.readDottedIp();
+    std::cout << "The IP address you entered is: ";
+    ip.displayAddress();
+    return 0;
+}
